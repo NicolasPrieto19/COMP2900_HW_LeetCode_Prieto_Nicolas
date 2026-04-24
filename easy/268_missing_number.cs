89504a7463fd837d8672c869338284c071a6d622
Nicolas_Prieto_Diaz <NPrieto5685@interbayamon.edu>
2026-04-24 13:47:07 -0400

    Publicacion de la solucion al problema 268_missing_number

--- easy/268_missing_number.cs
+++ easy/268_missing_number.cs
@@ -1,5 +1,48 @@
+using System;
+
+// Algoritmo que soluciona el problema.
 public class Solution
 {
-    public int hola{
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+
+        for (int i = 0; i <= n; i++)
+        {
+            bool found = false;
+
+            for (int j = 0; j < n; j++)
+            {
+                if (nums[j] == i)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+
+// Prueba para verificar el funcionamiento de la solucion.
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+
+        int[] nums1 = { 3, 0, 1 };
+        int[] nums2 = { 0, 1, 2, 3, 4, 6 };
+        int[] nums3 = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+
+        Console.WriteLine(sol.MissingNumber(nums1));
+        Console.WriteLine(sol.MissingNumber(nums2));
+        Console.WriteLine(sol.MissingNumber(nums3));
     }
 }
